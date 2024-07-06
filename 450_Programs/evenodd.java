62b493acf77488a411b44ceb7df92827cad62bd0
Prashant Dasnur <78973641+Professor1416@users.noreply.github.com>
2024-07-06 19:48:29 +0530

    evenodd.java
    This Java program determines whether a given number is even or odd.

--- 450_Programs/evenodd.java
+++ 450_Programs/evenodd.java
@@ -1,3 +1,5 @@
+//Java Even/Odd Number Program
+//This Java program determines whether a given number is even or odd.
 import java.util.Scanner;
 public class evenodd
 {
@@ -17,3 +19,31 @@ public class evenodd
 		sc.close();
 	}
 }
+
+/*
+>>User Input:
+System.out.println("Enter Number"); prompts the user to enter a number.
+int num = sc.nextInt(); reads the number from the user.
+
+>>Even/Odd Check:
+The if statement checks if the number is even by evaluating num % 2 == 0.
+
+If the number is even (num % 2 == 0 evaluates to true):
+System.out.println(num + " is an even number"); prints that the number is even.
+
+If the number is odd (num % 2 != 0):
+System.out.println(num + " is an odd number"); prints that the number is odd.
+
+>>Scanner Closure:
+sc.close(); closes the Scanner object to avoid resource leaks.
+
+>>Output
+Enter Number:
+4
+4 is an even number
+
+Enter Number:
+5
+5 is an odd number
+*/
+
