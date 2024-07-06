ad5bafb1842be5199b9d5b53c052340c84c7f665
Prashant Dasnur <78973641+Professor1416@users.noreply.github.com>
2024-07-06 19:29:44 +0530

    emp.java
    >>This Java program calculates the remaining amount of an employee's salary after applying a tax (if applicable) and a withdrawal amount.
    >>The program uses a Scanner object for user input and includes a condition to apply a tax if the basic salary is above a certain threshold (25,000).

--- 450_Programs/emp.java
+++ 450_Programs/emp.java
@@ -1,3 +1,6 @@
+//Java Employee Salary Program
+//This Java program calculates the remaining amount of an employee's salary after applying a tax (if applicable) and a withdrawal amount.
+//The program uses a Scanner object for user input and includes a condition to apply a tax if the basic salary is above a certain threshold (25,000).
 import java.util.Scanner;
 public class emp
 {
@@ -35,3 +38,35 @@ public class emp
 		sc.close();
 	}
 }
+
+/*
+>>Variable Declarations:
+double rem_amount; declares a variable to store the remaining amount.
+double tax = b_salary * 0.1; calculates the tax as 10% of the basic salary.
+
+>>Tax Application and Salary Calculation:
+The if statement checks if the basic salary is greater than or equal to 25,000.
+**If true:
+The tax is subtracted from the basic salary.
+The remaining amount is calculated by subtracting the withdrawal amount from the updated basic salary.
+The employee's name, updated basic salary, tax amount, and remaining amount are printed.
+**If false:
+The remaining amount is calculated by subtracting the withdrawal amount from the basic salary.
+The employee's name, basic salary, and remaining amount are printed.
+
+>>Scanner Closure:
+sc.close(); closes the Scanner object to avoid resource leaks.
+
+>>Output
+Enter Employee Name:
+Ajay
+Enter Employee Basic Salary:
+30000
+Enter Withdraw amount:
+5000
+Employee Name: Ajay
+Basic Salary: 27000.00 Rs
+Tax Amount: 3000.00 Rs
+Remaining Amount: 22000.00 Rs
+
+*/
