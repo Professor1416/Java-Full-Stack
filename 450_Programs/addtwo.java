2c34f1a9f01315c04b96268760a8fe4b47698d2d
Prashant Dasnur <78973641+Professor1416@users.noreply.github.com>
2024-07-06 18:09:17 +0530

    addtwo.java
    >>Variable Declaration and Initialization:
    
    int a = 10; declares an integer variable a and initializes it to 10.
    int b = 20; declares another integer variable b and initializes it to 20.
    
    >>Performing and Printing Arithmetic Operations:
    System.out.println("Addition is " + (a + b)); calculates the sum of a and b, and prints the result.
    System.out.println("Subtract is " + (a - b)); calculates the difference between a and b, and prints the result.
    System.out.println("Multiplication is " + (a * b)); calculates the product of a and b, and prints the result.
    System.out.println("Division is " + (a / b)); calculates the quotient of a divided by b, and prints the result. Note that since a and b are integers, this will perform integer division.
    
    >>Execution
    When you run this program, it will output:
    Addition is 30
    Subtract is -10
    Multiplication is 200
    Division is 0

--- 450_Programs/addtwo.java
+++ 450_Programs/addtwo.java
@@ -11,3 +11,24 @@ public class addtwo
 			
 	}
 }
+
+/*
+>>Variable Declaration and Initialization:
+
+int a = 10; declares an integer variable a and initializes it to 10.
+int b = 20; declares another integer variable b and initializes it to 20.
+
+>>Performing and Printing Arithmetic Operations:
+System.out.println("Addition is " + (a + b)); calculates the sum of a and b, and prints the result.
+System.out.println("Subtract is " + (a - b)); calculates the difference between a and b, and prints the result.
+System.out.println("Multiplication is " + (a * b)); calculates the product of a and b, and prints the result.
+System.out.println("Division is " + (a / b)); calculates the quotient of a divided by b, and prints the result. Note that since a and b are integers, this will perform integer division.
+
+>>Execution
+When you run this program, it will output:
+Addition is 30
+Subtract is -10
+Multiplication is 200
+Division is 0
+
+*/
