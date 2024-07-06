e04a6f04efea9ceab4ae7e1eb0d9edc68f7fc9fd
Prashant Dasnur <78973641+Professor1416@users.noreply.github.com>
2024-07-06 19:25:18 +0530

    divisor.java
    This Java program finds and displays all the divisors of a given number, counts the total number of divisors, and calculates the sum of these divisors.

--- 450_Programs/divisor.java
+++ 450_Programs/divisor.java
@@ -1,3 +1,5 @@
+//Java Divisor Program
+//This Java program finds and displays all the divisors of a given number, counts the total number of divisors, and calculates the sum of these divisors.
 import java.util.Scanner;
 public class divisor
 {
@@ -25,3 +27,44 @@ public class divisor
 		
 	}
 }
+
+
+/*
+>>Variable Declarations:
+Scanner sc = new Scanner(System.in); creates a Scanner object to read input from the standard input stream (keyboard).
+int num, i, cnt = 0, sum = 0; declares variables for the number to be checked (num), the loop counter (i), the count of divisors (cnt), and the sum of divisors (sum).
+
+>>User Input:
+System.out.println("Enter Number"); prompts the user to enter a number.
+num = sc.nextInt(); reads the number from the user.
+
+>>Divisor Calculation:
+System.out.println("Divisor Of " + num); prints a message indicating that the divisors of the entered number will be displayed.
+The for loop iterates from 1 to the entered number (num). For each iteration, it checks if the current value of i is a divisor of num by evaluating num % i == 0.
+
+>>Inside the Loop:
+If i is a divisor of num:
+System.out.println(i); prints the divisor.
+cnt++; increments the count of divisors.
+sum += i; adds the divisor to the sum of divisors.
+
+Output:
+System.out.println("Total Count OF Divisor " + cnt); prints the total count of divisors.
+System.out.println("Total Sum " + sum); prints the total sum of divisors.
+Scanner Closure:
+
+The program does not explicitly close the Scanner object, but it's a good practice to do so to avoid resource leaks. You can add sc.close(); 
+at the end of the main method.
+
+>>Output
+Enter Number
+6
+Divisors of 6:
+1
+2
+3
+6
+Total Count of Divisors: 4
+Total Sum of Divisors: 12
+
+*/
